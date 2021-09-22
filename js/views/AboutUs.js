70222f3dbfdbe5c950ddaace0bcd5d23dc70eaef
brellin <cekakid@gmail.com>
2021-09-21 23:10:23 -0500

    text in about

--- js/views/AboutUs.js
+++ js/views/AboutUs.js
@@ -9,7 +9,7 @@ export default class extends AbstractView {
     getHtml() {
         return `
             <p>We are proud Texans, living just outside of Abilene. Lily has become a great joy in our lives and we hope to provide a pet who will be a blessing to you as well!</p>
-            <p>If you are interested in purchasing a puppy, or simply want more information, please feel free to contact us <a href="mailto:jill@goldenglories.com">here</a>!</p>
+            <p>If you are interested in purchasing a puppy, or simply want more information, please feel free to email us <a href="mailto:jill@goldenglories.com">here</a>!</p>
         `;
     }
 
