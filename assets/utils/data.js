25ebfed75227c321813401fde0830738ab04f4aa
brellin <cekakid@gmail.com>
2021-09-29 01:24:23 -0500

    add links for ENS includes title for hover information

--- assets/utils/data.js
+++ assets/utils/data.js
@@ -1,5 +1,6 @@
 const ext_links = {
     bloodline: 'https://www.k9data.com/pedigree.asp?ID=1148115',
+    ens: 'https://www.akc.org/expert-advice/dog-breeding/breeder-puppy-socialization-early-neurological-stimulation/'
 };
 
 export {
