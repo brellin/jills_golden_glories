7e4f104c2d984b2fd9be304e3cf69095aa6e0fdb
brellin <cekakid@gmail.com>
2021-10-01 23:07:53 -0500

    relative reference to node module

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { Router } from 'router-vanilla';
+import { Router } from '../node_modules/router-vanilla';
 import { Home, RaisingPuppies, AboutTheBreed, AboutUs } from './views/index.js';
 
 let isWide = window.innerWidth > 800;
