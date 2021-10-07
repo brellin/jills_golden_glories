4e896cc550e2adff501652e8b65b24a713b61bc3
brellin <cekakid@gmail.com>
2021-10-07 15:55:08 -0500

    update email to display text for email

--- js/views/AboutUs.js
+++ js/views/AboutUs.js
@@ -9,7 +9,7 @@ export default class extends View {
     getHtml() {
         return `
             <p>We are proud Texans, living just outside of Abilene. Lily has become a great joy in our lives and we hope to provide a pet who will be a blessing to you as well!</p>
-            <p>If you are interested in purchasing a puppy, or simply want more information, please feel free to email us <a href="mailto:jill@goldenglories.com">here</a>!</p>
+            <p>If you are interested in purchasing a puppy, or simply want more information, please feel free to email us at <a href="mailto:jill@goldenglories.com">jill@goldenglories.com</a>!</p>
         `;
     }
 
