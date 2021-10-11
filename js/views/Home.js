ffd1edbb495ba36107218aa8a519f437f816a5ae
brellin <cekakid@gmail.com>
2021-10-11 11:19:12 -0500

    add spacer and additional text to home page

--- js/views/Home.js
+++ js/views/Home.js
@@ -8,7 +8,11 @@ export default class extends View {
 
     getHtml() {
         return `
-        <p>Welcome to Golden Glories. We breed English Cream Golden Retriever puppies. We are located in Tuscola, Texas, about 20 miles southwest of Abilene.</p>
+        <div class="Home">
+            <p>Welcome to Golden Glories. We breed English Cream Golden Retriever puppies. We are located in Tuscola, Texas, about 20 miles southwest of Abilene.</p>
+            <div class="spacer home"></div>
+            <p>We emphasize that we breed <i>English</i> Golden Retrievers. You can investigate the differences between American and English Golden Retrievers <a href="/about_the_breed" class="internal">here</a>. The pictures that you see of dogs on this website are taken by us. The adult dog you see is Lady Liliana Rae, the mother of your future puppies!</p>
+        </div>
         `;
     }
 
