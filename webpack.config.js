97a2e3f7a979e339e0c7c3180011e37904639678
brellin <cekakid@gmail.com>
2021-10-17 20:38:04 -0500

    remove package-lock and use style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 module.exports = {
     entry: './src/app.js',
-    mode: 'development',
+    mode: process.env.NODE_ENV,
     devServer: {
         port: 9455,
         static: path.resolve(__dirname, 'src'),
         open: true
     },
     plugins: [
-        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         })
@@ -23,9 +21,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
-                    process.env.NODE_ENV !== "production"
-                        ? "style-loader"
-                        : MiniCssExtractPlugin.loader,
+                    "style-loader",
                     "css-loader",
                     "sass-loader",
                 ],
