87641cf0b68286d36dc79eab60ce589f0141a6c0
brellin <cekakid@gmail.com>
2022-08-05 12:58:52 -0500

    crappy slideshow made

--- src/components/Pup/Pup.vue
+++ src/components/Pup/Pup.vue
@@ -4,13 +4,22 @@
     <button class="ellipsis">•••</button>
     <h2>{{ title }}</h2>
     <p>{{ sold ? "Sold" : "Not Sold" }}</p>
-    <PupPic
-      v-for="pic in pictures"
-      v-bind="pic"
-      @delete-pic="deletePic"
-      :id="_id"
-      :key="pic.public_id"
-    />
+    <button class="navigate decrement" :disabled="pictures.length < 2" @click="dAI">
+      {{ "<" }}
+    </button>
+    <div class="pic-con-con">
+      <PupPic
+        v-for="(pic, index) in pictures"
+        v-bind="pic"
+        @delete-pic="deletePic"
+        :activeImg="activeImg === index"
+        :id="_id"
+        :key="pic.public_id"
+      />
+    </div>
+    <button class="navigate increment" :disabled="pictures.length < 2" @click="iAI">
+      {{ ">" }}
+    </button>
   </article>
 </template>
 
@@ -25,6 +34,11 @@
       sold: Boolean,
       pictures: Array,
     },
+    data() {
+      return {
+        activeImg: 0,
+      };
+    },
     components: { PupPic },
     methods: {
       async deletePup() {
@@ -35,6 +49,12 @@
       },
       deletePic(pid) {
         this.$emit("delete-pic", this._id, pid);
+      },
+      iAI() {
+        this.activeImg = this.activeImg === this.pictures.length - 1 ? 0 : this.activeImg + 1;
+      },
+      dAI() {
+        this.activeImg = this.activeImg === 0 ? this.pictures.length - 1 : this.activeImg - 1;
       },
     },
   };
@@ -61,6 +81,36 @@
       top: 20px;
       left: 20px;
       z-index: 1;
+    }
+
+    button.navigate {
+      position: absolute;
+      border: none;
+      color: $gold;
+      text-shadow: 1px 1px black;
+      font-size: 5rem;
+      font-family: "Little Star Story";
+      top: 50%;
+      background: none;
+      outline: none;
+      cursor: pointer;
+
+      &:disabled {
+        color: gray;
+        cursor: not-allowed;
+      }
+
+      &.increment {
+        right: 5px;
+      }
+
+      &.decrement {
+        left: 5px;
+      }
+    }
+    div.pic-con-con {
+      @include flex(row, center, center, nowrap);
+      width: 100%;
     }
 
     input[type="text"],
--- src/components/Pup/PupPic.vue
+++ src/components/Pup/PupPic.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="pic-con-con">
-    <div class="pic-con">
-      <div class="head-con">
-        <div></div>
-        <h3>{{ readPublicId(public_id) }}</h3>
-        <button class="dimg" @click="deleteImage">X</button>
-      </div>
-      <img :src="url" :alt="title" :title="title" />
+  <div :class="`pic-con${activeImg ? ' active' : ''}`">
+    <div class="head-con">
+      <div></div>
+      <h3>{{ readPublicId(public_id) }}</h3>
+      <button class="dimg" @click="deleteImage">X</button>
     </div>
+    <img :src="url" :alt="title" :title="title" />
   </div>
 </template>
 
@@ -21,10 +19,13 @@
       url: String,
       title: String,
       public_id: String,
+      activeImg: Boolean,
     },
     methods: {
       async deleteImage() {
-        if (window.confirm(`Are you sure you want to delete ${readPublicId(this.public_id)}?`)) {
+        if (
+          window.confirm(`Are you sure you want to delete ${readPublicId(this.public_id)}?`)
+        ) {
           deletePicture(this.id, this.public_id);
           this.$emit("delete-pic", this.public_id);
         } else alert(`You have chosen not to delete ${readPublicId(this.public_id)}.`);
@@ -35,45 +36,47 @@
 </script>
 
 <style lang="scss">
-  div.pic-con-con {
-    @include flex(column, flex-start, center);
-    width: 100%;
+  div.pic-con {
+    position: relative;
+    @include card;
+    width: 0;
+    max-width: calc(80%);
+    padding: 0 0 15px;
+    background: $gold_input;
+    border-radius: 5px;
+    overflow: hidden;
+    // transform: scale(0);
+    transition: 0.3s ease;
 
-    div.pic-con {
-      position: relative;
-      @include card;
-      width: max-content;
-      max-width: calc(100% - 100px);
-      padding: 0 0 15px;
-      background: $gold_input;
-      border-radius: 5px;
-      overflow: hidden;
+    &.active {
+      width: 100%;
+      // transform: scale(1);
+    }
 
-      div.head-con {
-        @include flex($j: space-between, $a: center);
-        width: 100%;
-        background: $blue;
-        padding: 15px;
-        margin-bottom: 15px;
+    div.head-con {
+      @include flex($j: space-between, $a: center);
+      width: 100%;
+      background: $blue;
+      padding: 15px;
+      margin-bottom: 15px;
 
-        h3 {
-          margin: 10px;
-          color: $gold;
-        }
-
-        div {
-          width: 40px;
-        }
-
-        button.dimg {
-          @include spin-button;
-        }
+      h3 {
+        margin: 10px;
+        color: $gold;
       }
 
-      img {
-        width: 100%;
-        border-radius: 0;
+      div {
+        width: 40px;
       }
+
+      button.dimg {
+        @include spin-button;
+      }
+    }
+
+    img {
+      width: 100%;
+      border-radius: 0;
     }
   }
 </style>
