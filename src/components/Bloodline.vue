ea005901193169793d690d7e8c235733a381e05d
brellin <cekakid@gmail.com>
2022-08-21 15:10:16 -0500

    extract bloodline to component

---
+++ src/components/Bloodline.vue
@@ -0,0 +1,39 @@
+<template>
+  <article class="bloodline">
+    <h2 v-html="headerText" />
+    <img :src="imgSrc" :alt="imgAlt" :title="imgAlt" />
+    <a :href="bloodlink" target="_blank" rel="noopener noreferrer" class="slide">
+      View {{ pluralName }} Pedigree
+    </a>
+    <p v-if="additionalInfo" v-html="additionalInfo" />
+  </article>
+</template>
+
+<script>
+  export default {
+    name: "gg-bloodline",
+    props: {
+      callName: String,
+      headerText: String,
+      imgSrc: String,
+      imgAlt: String,
+      bloodlink: String,
+      pluralName: String,
+      additionalInfo: String,
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  article.bloodline {
+    @include card;
+
+    img {
+      width: 80%;
+    }
+
+    a {
+      margin-top: 15px;
+    }
+  }
+</style>
--- src/views/Bloodlines.vue
+++ src/views/Bloodlines.vue
@@ -1,31 +1,13 @@
 <template>
   <h1 v-html="heading" />
   <div class="sub-contain">
-    <div
-      class="bloodline"
-      v-for="{
-        callName,
-        headerText,
-        imgSrc,
-        imgAlt,
-        bloodlink,
-        pluralName,
-        additionalInfo,
-      } of bloodlines"
-      :key="callName"
-    >
-      <h2 v-html="headerText" />
-      <img :src="imgSrc" :alt="imgAlt" :title="imgAlt" />
-      <a :href="bloodlink" target="_blank" rel="noopener noreferrer" class="slide">
-        View {{ pluralName }} Pedigree
-      </a>
-      <p v-if="additionalInfo" v-html="additionalInfo" />
-    </div>
+    <Bloodline v-for="bl of bloodlines" v-bind="bl" :key="bl.callName" />
   </div>
 </template>
 
 <script>
   import { bloodlines, l } from "../assets/utils/data";
+  import Bloodline from "@/components/Bloodline.vue";
 
   export default {
     name: "gg-bloodlines",
@@ -35,19 +17,6 @@
         bloodlines,
       };
     },
+    components: { Bloodline },
   };
 </script>
-
-<style lang="scss" scoped>
-  div.bloodline {
-    @include card;
-
-    img {
-      width: 80%;
-    }
-
-    a {
-      margin-top: 15px;
-    }
-  }
-</style>
