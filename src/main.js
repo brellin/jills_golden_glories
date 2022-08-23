10cb8452feb4b4e9072bfb351748fe41307c905d
brellin <cekakid@gmail.com>
2022-08-22 23:20:39 -0500

    use loader to await server

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import App from './App.vue';
 import router from './routes';
 import store from './plugins/vuex';
 import './assets/styles/base.scss';
-
-
+import DotLoader from 'vue-spinner/src/DotLoader.vue';
 
 const app = createApp(App);
 app.config.globalProperties.notFirstVisit = JSON.parse(localStorage.getItem('notFirstVisit'));
+app.component('DotLoader', DotLoader);
 app
     .use(router)
     .use(store)
--- src/views/Bloodlines.vue
+++ src/views/Bloodlines.vue
@@ -1,14 +1,15 @@
 <template>
   <h1 v-html="heading" />
-  <div class="sub-contain" v-if="bloodlines.length">
+  <div class="sub-contain" v-if="!loading">
     <Bloodline v-for="bl of bloodlines" v-bind="bl" :key="bl._id" />
   </div>
+  <DotLoader v-else color="#ffcc00" />
 </template>
 
 <script>
   import axios from "../plugins/axios";
   import { l } from "../assets/utils/data";
-  import Bloodline from "@/components/Bloodline.vue";
+  import Bloodline from "../components/Bloodline.vue";
 
   export default {
     name: "gg-bloodlines",
@@ -16,11 +17,13 @@
       return {
         heading: `${l("B")}loodlines`,
         bloodlines: [],
+        loading: true,
       };
     },
     async mounted() {
       const { data } = await axios.get("/bloodlines");
       this.bloodlines = data.sort((a, b) => (a._id < b._id ? -1 : a._id > b._id ? 1 : 0));
+      this.loading = false;
     },
     components: { Bloodline },
   };
