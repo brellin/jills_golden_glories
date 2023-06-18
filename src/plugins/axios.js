cecb9a1997acff427d3b535fc8b4cdaf900c3d65
brellin <cekakid@gmail.com>
2023-06-18 02:39:09 -0600

    secured again

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-axios.defaults.baseURL = /localhost/.test(window.location.host) ? 'http://localhost:9455' : 'http://server.goldenglories.com';
+axios.defaults.baseURL = /localhost/.test(window.location.host) ? 'http://localhost:9455' : 'https://server.willumstead.com';
 if (Cookies.get('token')) axios.defaults.headers.Authorization = Cookies.get('token');
 
 export default axios;
