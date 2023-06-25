e1c4f7b4cbce7c78cb6e4d8ef4ca46e029cd4a16
Will Umstead <william.c.umstead@gmail.com>
2023-06-25 13:58:47 -0600

    change to use new prefix

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-axios.defaults.baseURL = /localhost/.test(window.location.host) ? 'http://localhost:9455' : 'https://server.willumstead.com';
+axios.defaults.baseURL = /localhost/.test(window.location.host) ? 'http://localhost:9455' : 'https://server.willumstead.com/goldenglories';
 if (Cookies.get('token')) axios.defaults.headers.Authorization = Cookies.get('token');
 
 export default axios;
